c9ecfdc28c54c20a0940a374c18f5e5e5506aee4
Peacoke <codewar@163.com>
2021-07-10 14:36:18 +0800

    初始化项目

--- src/main/java/com/work/conroller/WeixinController.java
+++ src/main/java/com/work/conroller/WeixinController.java
@@ -1,9 +1,11 @@
 package com.work.conroller;
 
+import com.work.service.GirlService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Peacoke on 2018/7/29.
@@ -17,5 +19,18 @@ public class WeixinController {
         System.out.println("进入auth方法。。。");
         //System.out.println("code:"+code);
     }
+
+    @Resource
+    GirlService girlService;
+
+    @GetMapping("/insert")
+    public void insert(){
+        System.out.println("进入auth方法。。。");
+        //System.out.println("code:"+code);
+        girlService.insertTwo();
+
+    }
+
+
 
 }
